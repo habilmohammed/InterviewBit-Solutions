84dda6f1f4ddaa75d65a6b5c5592e22482ca6411
Habil Mohammed <hmohammed@rdalabs.com>
2018-02-26 12:58:41 +0530

    Solved Problems in Array section.
    Solved Merge Intervals.

---
+++ InterviewBit/src/com/interviewbit/habilmohammed/arrays/model/Interval.java
@@ -0,0 +1,33 @@
+package com.interviewbit.habilmohammed.arrays.model;
+
+public class Interval {
+  int start;
+  int end;
+  
+  public int getStart() {
+    return start;
+  }
+  public void setStart(int start) {
+    this.start = start;
+  }
+  public int getEnd() {
+    return end;
+  }
+  public void setEnd(int end) {
+    this.end = end;
+  }
+  
+  public Interval() {
+   start = 0;
+   end = 0;
+  }
+  public Interval (int start , int end ) {
+    this.start = start;
+    this.end = end;
+  }
+  
+  @Override
+  public String toString() {
+    return "Interval [start=" + start + ", end=" + end + "]";
+  }
+}
--- InterviewBit/src/com/interviewbit/habilmohammed/sortingAlgorithms/test.java
+++
@@ -1,5 +0,0 @@
-package com.interviewbit.habilmohammed.sortingAlgorithms;
-
-public class test {
-
-}
